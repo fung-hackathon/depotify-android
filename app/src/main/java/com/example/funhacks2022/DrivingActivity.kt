8e292dc017a5ce0c2d1608cf2e3fbd0bfa89eb81
Yourein <rein.you.3.1@gmail.com>
2022-10-03 23:10:03 +0900

    [add] Created mock DrivingActivity, no buttons work.

--- app/src/main/java/com/example/funhacks2022/DrivingActivity.kt
+++ app/src/main/java/com/example/funhacks2022/DrivingActivity.kt
@@ -3,11 +3,20 @@ package com.example.funhacks2022
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Outline
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.funhacks2022.ui.theme.FunHacks2022Theme
 
 class DrivingActivity : ComponentActivity() {
@@ -30,5 +39,39 @@ class DrivingActivity : ComponentActivity() {
 
 @Composable
 fun drivingComposable(){
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("送迎中です", fontSize = 50.sp)
 
+        Spacer(modifier = Modifier.padding(100.dp))
+
+        Button(
+            onClick = {/* TODO */},
+            modifier = Modifier.size(width = 275.dp, height = 50.dp),
+            shape = RoundedCornerShape(50.dp)
+        ){
+            Text(text = "送迎を終了", fontSize = 16.sp)
+        }
+
+        Spacer(modifier = Modifier.padding(20.dp))
+
+        Button(
+            onClick = {/* TODO */ },
+            modifier = Modifier.size(width = 275.dp, height = 50.dp),
+            shape = RoundedCornerShape(50.dp)
+        ){
+            Text(text = "送迎を中止", color = Color.Red, fontSize = 16.sp)
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    FunHacks2022Theme {
+        drivingComposable()
+    }
 }
